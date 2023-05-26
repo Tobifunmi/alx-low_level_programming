81064e88748b825a6fdc03da95e08cd79eb3de92
Tobifunmi <tobex13@gmail.com>
2023-05-27 00:44:39 +0100

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Starting point
+ * Return: Zero
+ */
+
+int main(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
