836199e886ddc8ae8c5f2b538ecb0d03110775f3
Tobifunmi <tobex13@gmail.com>
2023-05-27 00:30:04 +0100

    PRINTING alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,7 +6,7 @@
 
 int main(void)
 {
-	char aplh[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
