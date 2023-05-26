16a5bffcbd2565675de2faa260d255f766a406be
Tobifunmi <tobex13@gmail.com>
2023-05-26 21:19:12 +0100

    Printing Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main - Starting point of the code
+ * Return: Zero
+ */
+
+int main(void)
+{
+	char aph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(aph[26]);
+	}
+		putchar('\n');
+		return (0);
+}
