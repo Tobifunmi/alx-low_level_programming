c748923e460dbdfcdc7bbc37bf1c6fc6a69cbf15
Tobifunmi <tobex13@gmail.com>
2023-05-30 17:26:12 +0100

    Print alphabet ten times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@
 
 void print_alphabet_x10(void)
 {
-	int i;
+	int i, n;
 
 	for (n = 0; n < 11; n++)
 	{
 		for (i = 97; i < 123; i++)
 		{
-			print(i);
+			putchar(i);
 		}
 		putchar('\n');
 	}
